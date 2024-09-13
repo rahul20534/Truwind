212262606815121e52f841c0d47b5a9b913e18f3
Beingrahuul . <beingrahuul.@Beingrahuuls-MacBook-Air.local>
2024-09-13 19:52:38 +0530

    optimization

--- src/components/ValueSection/Value.jsx
+++ src/components/ValueSection/Value.jsx
@@ -1,86 +1,89 @@
 import React from 'react'
-import Styles from './Value.module.css';
-import Humanlogo from '../../Icon/human.jpg';
-import Circularlogo from '../../Icon/circular.jpg';
-import Arrowlogo from '../../Icon/arrow.jpg';
+import styles from './Value.module.css';
+
+//icons
+import EXPERT from '../../Icon/Expertise.png';
+import Process from '../../Icon/Process.png';
+import Motto from '../../Icon/Motto.png';
+import Purpose from '../../Icon/Purpose.png';
+
 
 const Value = () => {
-    return (
-        <>
-            <div className={Styles.value}>
-                <h2>Our Values</h2>
+  return (
 
-                <div className={Styles.mainvalue}>
-                    <div className={Styles.heading}>
-                        <h1>We promise the best </h1>
-                        <h1>values in a community</h1>
-                    </div>
+    <div className={styles.valueContainer}>
+      <h2>Our Values</h2>
 
+      <div className={styles.mainContainer}>
+        <div className={styles.heading}>
+          <h1>We promise the best values in a community</h1>
+        </div>
 
+        <div className={styles.ValueSection}>
 
-                    <div className={Styles.mainvalue_section}>
-                        <div className={Styles.value_row1}>
-                            <div className={Styles.expertise}>
-                                <img src={Humanlogo} alt="Logo" />
-                                <div className={Styles.expertise_section}>
-                                    <h1>Expertise</h1>
-                                    <p>Truward is a community-driven conscious business education company
-                                        dedicated to catalysing profound change in the business landscape.
-                                    </p>
-
-                                </div>
-
-                            </div>
-
-                            <div className={Styles.process}>
-                                <img src={Circularlogo} alt="Logo" />
-                                <div className={Styles.process_section}>
-                                    <h1>Process</h1>
-                                    <p>Truward is a community-driven conscious business education company
-                                        dedicated to catalysing profound change in the business landscape.
-                                    </p>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                        <div className={Styles.value_row2}>
-                            <div className={Styles.purpose}>
-                                <img src={Arrowlogo} alt="Logo" />
-                                <div className={Styles.purpose_section}>
-                                    <h1>Purpose</h1>
-                                    <p>Truward is a community-driven conscious business education company
-                                        dedicated to catalysing profound change in the business landscape.
-                                    </p>
-                                </div>
-
-                            </div>
-
-                            <div className={Styles.motto}>
-                                <img src={Circularlogo} alt="Logo" />
-                                <div className={Styles.motto_section}>
-                                    <h1>Motto</h1>
-                                    <p>Truward is a community-driven conscious business education company
-                                        dedicated to catalysing profound change in the business landscape.
-                                    </p>
-
-                                </div>
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
+          <div className={styles.part}>
+            <div className={styles.value}>
+              <div className={styles.logoContainer}>
+                <img src={EXPERT} alt="Logo" />
+              </div>
+              <div className={styles.valueDesc}>
+                <h1>Expertise</h1>
+                <p>
+                  Truward is a community-driven conscious business education company
+                  dedicated to catalysing profound change in the business landscape.
+                </p>
+              </div>
             </div>
 
+            <div className={styles.value}>
+              <div className={styles.logoContainer}>
+                <img src={Process} alt="Logo" />
+              </div>
+              <div className={styles.valueDesc}>
+                <h1>Process</h1>
+                <p>
+                  Truward is a community-driven conscious business education company
+                  dedicated to catalysing profound change in the business landscape.
+                </p>
+              </div>
+            </div>
 
-        </>
-    )
+          </div>
+
+          <div className={styles.part}>
+
+            <div className={styles.value}>
+              <div className={styles.logoContainer}>
+                <img src={Purpose} alt="Logo" />
+              </div>
+              <div className={styles.valueDesc}>
+                <h1>Purpose</h1>
+                <p>
+                  Truward is a community-driven conscious business education company
+                  dedicated to catalysing profound change in the business landscape.
+                </p>
+              </div>
+            </div>
+
+            <div className={styles.value}>
+              <div className={styles.logoContainer}>
+                <img src={Motto} alt="Logo" />
+              </div>
+              <div className={styles.valueDesc}>
+                <h1>Motto</h1>
+                <p>
+                  Truward is a community-driven conscious business education company
+                  dedicated to catalysing profound change in the business landscape.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
 }
 
 export default Value;
