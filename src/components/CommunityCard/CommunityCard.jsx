94777614df22e9ea1903e138dc14a7ddd72f7b86
Beingrahuul . <bstshownow@gmail.com>
2025-02-23 04:57:36 +0530

    changes

--- src/components/CommunityCard/CommunityCard.jsx
+++ src/components/CommunityCard/CommunityCard.jsx
@@ -6,36 +6,33 @@ import Logo from '../../Icon/Truwind.svg';
 
 const CommunityCard = () => {
     return (
-        <>
-            <div className={Styles.community}>
-
-                <div className={Styles.text}>
-                    <div className={Styles.logo}>
-                        <img src={Logo} />
-                        <h2>Community</h2>
-
-                    </div>
-
-                    <h1>Join Our Community
-                        And Unleash Your Potential
-                    </h1>
-                    <p>That will help you to unleash your superpowers Truward is a
-                        community-driven conscious business education company dedicated
-                        to catalysing profound change in the business landscape.
-                    </p>
-
-                    <button>Explore More <img src={arrow} /></button>
+        <div className={Styles.community}>
+            <div className={Styles.text}>
+                <div className={Styles.logo}>
+                    <img src={Logo} />
+                    <h2>Community</h2>
 
                 </div>
 
-                <div className={Styles.img}>
-                    <img src={image} />
-                </div>
+                <h1>Join Our Community
+                    And Unleash Your Potential
+                </h1>
+                <p>That will help you to unleash your superpowers Truward is a
+                    community-driven conscious business education company dedicated
+                    to catalysing profound change in the business landscape.
+                </p>
+
+                <div className={Styles.btn}>Explore More <img src={arrow} /></div>
 
             </div>
 
+            <div className={Styles.img}>
+                <img src={image} />
+            </div>
 
-        </>
+        </div>
+
+
     )
 }
 
--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -5,36 +5,32 @@ import forward from '../../Icon/forward.svg';
 
 const Event = () => {
   return (
+    <div className={Styles.event}>
+      <h2>Events</h2>
+      <h1>Upcoming Events</h1>
+      <p>Join our exclusive, high-impact events designed to bring changemakers together. Hear from thought leaders, collaborate with conscious entrepreneurs, and learn how to integrate conscious practices into your work.
+      </p>
 
-    <>
-      <div className={Styles.event}>
-        <h2>Events</h2>
-        <h1>Upcoming Events</h1>
-        <p>Join our exclusive, high-impact events designed to bring changemakers together. Hear from thought leaders, collaborate with conscious entrepreneurs, and learn how to integrate conscious practices into your work.
-        </p>
+      <div className={Styles.event_image}>
+        <img src={Image} alt="IMAGE" />
 
-        <div className={Styles.event_image}>
-          <img src={Image} alt="IMAGE" />
+        <div className={Styles.event_text}>
+          <h1>Gathering and Dinner Table</h1>
+          <h2>Thursday, December 19, 2024</h2>
+          <p>Let's know each other better and get a chance for networking.
+            This is a sample text that will describe what this event is all about.
+          </p>
 
-          <div className={Styles.event_text}>
-            <h1>Gathering and Dinner Table</h1>
-            <h2>Thursday, December 19, 2024</h2>
-            <p>Let’s know each other better and get a chance for networking.
-              This is a sample text that will describe what this event is all about.
-            </p>
-
-            <div className={Styles.btn}>
-              <button>Book Now <img src={forward}/></button>
-              <h1>Explore events</h1>
-            </div>
-
+          <div className={Styles.btn}>
+            <button>Book Now <img src={forward} /></button>
+            <h1>Explore events</h1>
           </div>
 
         </div>
 
       </div>
 
-    </>
+    </div>
   )
 }
 export default Event
