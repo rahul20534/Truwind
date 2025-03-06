6b4bbc661f627b0643c60b30b6319991ad2a0ba2
Rahul <88982398+rahul20534@users.noreply.github.com>
2025-03-06 15:00:08 +0530

    changes

--- src/components/Community_Componant/CommunityHeading/CommunityHeading.jsx
+++ src/components/Community_Componant/CommunityHeading/CommunityHeading.jsx
@@ -3,14 +3,15 @@ import Styled from './CommunityHeading.module.css';
 
 import img1 from '../../../Image/1_3.png'
 import img2 from '../../../Image/1_4.png'
-import image from '../../../Image/communitybanner.png'
+import image from '../../../Image/12.png'
 
 
 const CommunityHeading = () => {
     return (
-        <div className={Styled.communityheading}>
-            <div className={Styled.main}>
-                <img src={image} alt="" />
+        <div className={Styled.comm}>
+            <img src={image} alt="" />
+            <div className={Styled.text}>
+                <h1>Truward Community</h1>
             </div>
         </div>
     )
